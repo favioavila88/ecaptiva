936263c24ba5875eeffcd3cb80fdcc9d23a914ab
Favio <favio.avila.perez@gmail.com>
2023-03-06 12:39:25 -0400

    deled HomeRow7

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import HomeRow_1 from '../organisms/home/HomeRow_1'
 import HomeRow_2 from '../organisms/home/HomeRow_2'
 import HomeRow_3 from '../organisms/home/HomeRow_3'
 import HomeRow_6 from '../organisms/home/HomeRow_6'
-import HomeRow_7 from '../organisms/home/HomeRow_7'
 import HomeRow_8 from '../organisms/home/HomeRow_8'
 
 const HomePage = () => {
@@ -15,7 +14,7 @@ const HomePage = () => {
           <HomeRow_2 />
           <HomeRow_3 />
           <HomeRow_6 />
-          <HomeRow_7 />
+
           <HomeRow_8 />
         </div>
       </div>
