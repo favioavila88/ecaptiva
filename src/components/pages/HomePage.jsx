c82a50121bf2c393a3340b3d85c716f468273ffe
Favio <favio.avila.perez@gmail.com>
2023-03-06 10:21:29 -0400

    coment 4

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,35 +1,12 @@
 import React from 'react'
-import HomeRow_1 from '../organisms/home/homeRow_1'
-import HomeRow_2 from '../organisms/home/homeRow_2'
-import HomeRow_3 from '../organisms/home/homeRow_3'
-import HomeRow_4 from '../organisms/home/homeRow_4'
-import HomeRow_5 from '../organisms/home/homeRow_5'
-import HomeRow_6 from '../organisms/home/homeRow_6'
-import HomeRow_7 from '../organisms/home/homeRow_7'
-import HomeRow_8 from '../organisms/home/homeRow_8'
-import HomeRow_9 from '../organisms/home/homeRow_9'
-import HomeRow_10 from '../organisms/home/homeRow_10'
-import HomeRow_11 from '../organisms/home/homeRow_11'
 
 const HomePage = () => {
   return (
     <div>
       <div className='home'>
         < div className='home-container'>
-          <HomeRow_1 />
-          <HomeRow_2 />
-          <HomeRow_3 />
-          <HomeRow_4 />
-          <HomeRow_5 />
-          <HomeRow_6 />
-          <HomeRow_7 />
-          <HomeRow_8 />
-          <HomeRow_9 />
-          <HomeRow_10 />
-          <HomeRow_11 />
         </div>
       </div>
-
     </div>
   )
 }
