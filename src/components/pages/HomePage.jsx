7bb89bb4c60e70e17bbd48753902c68014e664e5
Favio <favio.avila.perez@gmail.com>
2023-03-06 12:41:58 -0400

    added HomeRow4

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import HomeRow_2 from '../organisms/home/HomeRow_2'
 import HomeRow_3 from '../organisms/home/HomeRow_3'
 import HomeRow_6 from '../organisms/home/HomeRow_6'
 import HomeRow_8 from '../organisms/home/HomeRow_8'
+import HomeRow_4 from '../organisms/home/homeRow_4'
 
 const HomePage = () => {
   return (
@@ -13,8 +14,8 @@ const HomePage = () => {
           <HomeRow_1 />
           <HomeRow_2 />
           <HomeRow_3 />
+          <HomeRow_4 />
           <HomeRow_6 />
-
           <HomeRow_8 />
         </div>
       </div>
