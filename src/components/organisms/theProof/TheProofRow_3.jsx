c5d3a3b1e1865503e27065a775409bb61fd9dc50
Favio <favio.avila.perez@gmail.com>
2023-03-07 17:13:03 -0400

    added theProofRow_3

--- src/components/organisms/theProof/TheProofRow_3.jsx
+++ src/components/organisms/theProof/TheProofRow_3.jsx
@@ -1,142 +1,115 @@
 import React, { useState } from 'react'
 import '../../Styles/pages/theProof/TheProofRow_3.css';
-import img1 from '../../../assets/organisms/theProof/01-emmys-2-500x360.jpg'
-import img2 from '../../../assets/organisms/theProof/02-emmys-fyc-500x360.jpg'
-import img3 from '../../../assets/organisms/theProof/03-hoopla-500x360.jpg'
-import img4 from '../../../assets/organisms/theProof/04-muniaxis-1-500x360.jpg'
-import img5 from '../../../assets/organisms/theProof/05-coldwell-banker-500x360.jpg'
-import img6 from '../../../assets/organisms/theProof/06-endemol-500x360.jpg'
-import img7 from '../../../assets/organisms/theProof/07-nascar-500x360.jpg'
-import img8 from '../../../assets/organisms/theProof/08-agilent-tech-1-500x360.jpg'
-import img9 from '../../../assets/organisms/theProof/09-mcafee-500x360.jpg'
-import img10 from '../../../assets/organisms/theProof/10-suzuki-500x360.jpg'
-import img11 from '../../../assets/organisms/theProof/11-verizon-500x360.jpg'
-import img12 from '../../../assets/organisms/theProof/12-conviva-500x360.jpg'
-import img13 from '../../../assets/organisms/theProof/13-manpower-500x360.jpg'
-import img14 from '../../../assets/organisms/theProof/14-honda-500x360.jpg'
-import img15 from '../../../assets/organisms/theProof/15-petrobras-500x360.jpg'
-import img16 from '../../../assets/organisms/theProof/16-visa-500x360.jpg'
-import img17 from '../../../assets/organisms/theProof/17-arkadium-5-500x360.jpg'
-import img18 from '../../../assets/organisms/theProof/18-junglebat-500x360.jpg'
-import img19 from '../../../assets/organisms/theProof/19-flixster-500x360.jpg'
-import img20 from '../../../assets/organisms/theProof/20-panasonic-500x360.jpg'
-import img22 from '../../../assets/organisms/theProof/22-nissan-500x360.jpg'
-import img23 from '../../../assets/organisms/theProof/23-continuumfi-500x360.jpg'
-import img24 from '../../../assets/organisms/theProof/24-mym-4-500x360.jpg'
-import img25 from '../../../assets/organisms/theProof/25-trinity-2-500x360.jpg'
-import img26 from '../../../assets/organisms/theProof/26-oasis-collections-3-500x360.jpg'
-import img27 from "../../../assets/organisms/theProof/27-crackle-500x360.jpg"
-
 
 const menu = [
     {
         id: 1,
-        image: { img27 },
+        image: "https://www.truelogic.io/hubfs/images/proof/27-crackle-500x360.jpg",
         name: "Crackle",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 2,
-        image: { img26 },
+        image: "https://www.truelogic.io/hubfs/images/proof/26-oasis-collections-3-500x360.jpg",
         name: "Oasis Collections",
         type: ("ecommerce" || "web-site"),
         description: "Ecommerce / Web Site"
     },
     {
         id: 3,
-        image: { img25 },
+        image: "https://www.truelogic.io/hubfs/images/proof/25-trinity-2-500x360.jpg",
         name: "Trinity Wall Street Church",
         type: "web-site",
         description: "Web Site"
     },
     {
         id: 4,
-        image: { img24 },
+        image: "https://www.truelogic.io/hubfs/images/proof/24-mym-4-500x360.jpg",
         name: "M&M",
         type: "banners-campaign",
         description: "Banners Campaign"
     },
     {
         id: 5,
-        image: { img17 },
+        image: "https://www.truelogic.io/hubfs/images/proof/17-arkadium-5-500x360.jpg",
         name: "Arkadium",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 6,
-        image: { img4 },
+        image: "https://www.truelogic.io/hubfs/images/proof/04-muniaxis-1-500x360.jpg",
         name: "Muniaxis",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 7,
-        image: { img1 },
+        image: "https://www.truelogic.io/hubfs/images/proof/01-emmys-2-500x360.jpg",
         name: "Emmys",
         type: "mobile-app",
         description: "Mobile App"
     },
     {
         id: 8,
-        image: { img2 },
+        image: "https://www.truelogic.io/hubfs/images/proof/02-emmys-fyc-500x360.jpg",
         name: "Emmys FYC",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 9,
-        image: { img6 },
+        image: "https://www.truelogic.io/hubfs/images/proof/06-endemol-500x360.jpg",
         name: "Endemol",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 10,
-        image: { img3 },
+        image: "https://www.truelogic.io/hubfs/images/proof/03-hoopla-500x360.jpg",
         name: "Hoopla",
         type: "ecommerce" || "web-site",
         description: "Ecommerce / Web Site"
     },
     {
         id: 11,
-        image: { img5 },
+        image: "https://www.truelogic.io/hubfs/images/proof/05-coldwell-banker-500x360.jpg",
         name: "Coldwell Banker",
         type: "web-site",
         description: "Web Site"
     },
     {
         id: 12,
-        image: { img7 },
+        image: "https://www.truelogic.io/hubfs/images/proof/07-nascar-500x360.jpg",
         name: "Nascar",
         type: "banners-campaign",
         description: "Banners Campaign"
     },
     {
         id: 13,
-        image: { img8 },
+        image: "https://www.truelogic.io/hubfs/images/proof/08-agilent-tech-1-500x360.jpg",
         name: "Agilent Technologies",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 14,
-        image: { img9 },
+        image: "https://www.truelogic.io/hubfs/images/proof/09-mcafee-500x360.jpg",
         name: "McAfee",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 15,
-        image: { img10 },
+        image: "https://www.truelogic.io/hubfs/images/proof/10-suzuki-500x360.jpg",
         name: "Suzuki",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 16,
-        image: { img11 },
+        image: "https://www.truelogic.io/hubfs/images/proof/11-verizon-500x360.jpg",
         name: "Verizon",
         type: "web-app",
         description: "Web App"
@@ -144,70 +117,70 @@ const menu = [
 
     {
         id: 17,
-        image: { img12 },
+        image: "https://www.truelogic.io/hubfs/images/proof/12-conviva-500x360.jpg",
         name: "Conviva",
         type: "mobile-app",
         description: "Mobile App"
     },
     {
         id: 18,
-        image: { img13 },
+        image: "https://www.truelogic.io/hubfs/images/proof/13-manpower-500x360.jpg",
         name: "Manpower",
         type: "interactive-kiosk",
         description: "Interactive Kiosk"
     },
     {
         id: 19,
-        image: { img14 },
+        image: "https://www.truelogic.io/hubfs/images/proof/14-honda-500x360.jpg",
         name: "Honda",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 20,
-        image: { img15 },
+        image: "https://www.truelogic.io/hubfs/images/proof/15-petrobras-500x360.jpg",
         name: "Petrobras",
         type: "ecommerce" || "web-site",
         description: "Ecommerce / Web Site"
     },
     {
         id: 22,
-        image: { img16 },
+        image: "https://www.truelogic.io/hubfs/images/proof/16-visa-500x360.jpg",
         name: "Visa",
         type: "web-site",
         description: "Web Site"
     },
     {
         id: 23,
-        image: { img18 },
+        image: "https://www.truelogic.io/hubfs/images/proof/18-junglebat-500x360.jpg",
         name: "Junglebat",
         type: "banners-campaign",
         description: "Banners Campaign"
     },
     {
         id: 24,
-        image: { img19 },
+        image: "https://www.truelogic.io/hubfs/images/proof/19-flixster-500x360.jpg",
         name: "Flixster",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 25,
-        image: { img20 },
+        image: "https://www.truelogic.io/hubfs/images/proof/20-panasonic-500x360.jpg",
         name: "Panasonic",
         type: "web-app",
         description: "Web App"
     },
     {
         id: 26,
-        image: { img22 },
+        image: "https://www.truelogic.io/hubfs/images/proof/22-nissan-500x360.jpg",
         name: "Nissan",
         type: "mobile-app",
         description: "Mobile App"
     },
     {
         id: 27,
-        image: { img23 },
+        image: "https://www.truelogic.io/hubfs/images/proof/23-continuumfi-500x360.jpg",
         name: "Continuumfi",
         type: "ecommerce" || "web-site",
         description: "Ecommerce / Web Site"
