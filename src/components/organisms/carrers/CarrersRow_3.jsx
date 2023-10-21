d053e0fba565a06b9196918fe742c81ee1b1fc66
RichardEcosm <richard_ecos@hotmail.com>
2023-10-21 00:12:47 -0400

    Adding new components to render careers

--- src/components/organisms/carrers/CarrersRow_3.jsx
+++ src/components/organisms/carrers/CarrersRow_3.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import '../../Styles/pages/carrers/CarrersRow_3.css'
-import careers_02 from '../../../assets/organisms/carrers/carrersRow_3/careers-02.png'
+import React from "react";
+import "../../Styles/pages/carrers/CarrersRow_3.css";
+import careers_02 from "../../../assets/organisms/carrers/carrersRow_3/careers-02.png";
+import CareerBase from "./CareerBase";
 
 const CarrersRow_3 = () => {
   return (
-    <div className='ca-row-3'>
-    <div className='ca-row-3-container'>
-        <div className='ca-row-3-content'>
-            <div className='ca-row-3-img'><img src={careers_02} alt="careers_02" /></div>
-            <div className='ca-row-3-content-text'>
-                <h2>Be part of a community</h2>
-                <p>Even remotely, we are a great team. We develop
-                    applications, exchange knowledge and, in this
-                    way, we are able to evolve together.</p>
-            </div>
-        </div>
-    </div>
-</div>
-  )
-}
+    <CareerBase
+      text="Even remotely, we are a great team. We develop applications, exchange knowledge and, in this way, we are able to evolve together."
+      title="Be part of a community"
+      image={careers_02}
+      reverse={true}
+    ></CareerBase>
+  );
+};
 
-export default CarrersRow_3
+export default CarrersRow_3;
--- src/components/organisms/carrers/CarrersRow_4.jsx
+++ src/components/organisms/carrers/CarrersRow_4.jsx
@@ -1,26 +1,17 @@
-import React from 'react'
-import '../../Styles/pages/carrers/CarrersRow_4.css'
-import careers_03 from '../../../assets/organisms/carrers/carrersRow_4/careers-03.png'
+import React from "react";
+import "../../Styles/pages/carrers/CarrersRow_4.css";
+import careers_03 from "../../../assets/organisms/carrers/carrersRow_4/careers-03.png";
+import CareerBase from "./CareerBase";
 
 const CarrersRow_4 = () => {
-    return (
-        <div className='ca-row-4'>
-            <div className='ca-row-4-container'>
-                <div className='ca-row-4-content'>
-                    <div className='ca-row-4-content-text'>
-                        <h2>Work from anywhere</h2>
-                        <p>Freedom is one of the highlights of working at
-                            Truelogic. Here you have an advantageous benefits
-                            package, receive your payment in dollars, have
-                            PTO - Paid Time Off and can manage your day to day
-                            the way you want.</p>
-                    </div>
-                    <div className='ca-row-4-img'><img src={careers_03} alt="careers_03" /></div>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <CareerBase
+      text="Freedom is one of the highlights of working at Truelogic. Here you have an advantageous benefits package, receive your payment in dollars, have PTO - Paid Time Off and can manage your day to day the way you want."
+      title="Work from anywhere"
+      image={careers_03}
+      reverse={false}
+    ></CareerBase>
+  );
+};
 
-}
-
-export default CarrersRow_4
+export default CarrersRow_4;
