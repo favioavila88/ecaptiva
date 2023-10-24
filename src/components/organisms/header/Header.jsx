a2eb189ab1a27323493af9a6fa3ab19f29cda8c2
RichardEcosm <richard_ecos@hotmail.com>
2023-10-23 22:25:23 -0400

    Updating styles to add transition on header

--- src/components/organisms/header/Header.jsx
+++ src/components/organisms/header/Header.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import Logo from '../../molecules/header/Logo'
-import Navbar from '../../molecules/header/Navbar'
-import '../../Styles/organisms/header/Header.css'
+import React, { useState } from "react";
+import Logo from "../../molecules/header/Logo";
+import Navbar from "../../molecules/header/Navbar";
+import "../../Styles/organisms/header/Header.css";
 
 const Header = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY > 80) {
+      console.log(window.scrollY);
+      setNavbar(true);
+    } else {
+      console.log("menor");
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
   return (
-    <div>
-      <div className='header'>
-        <div className='contain-header'>
+    <div className="header-1">
+      <div>
+        <div className={navbar ? "contain-header active" : "contain-header"}>
           <Logo />
           <Navbar />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/pages/GetAnEstimate.jsx
+++ src/components/pages/GetAnEstimate.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import GetAnEstimateRow_2 from '../organisms/getAnEstimate/GetAnEstimateRow_2'
-import ContactUsRow_3 from '../organisms/contactUs/ContactUsRow_3'
-import ContactUsRow_1 from '../organisms/contactUs/ContactUsRow_1'
-
+import React from "react";
+import GetAnEstimateRow_2 from "../organisms/getAnEstimate/GetAnEstimateRow_2";
+import ContactUsRow_3 from "../organisms/contactUs/ContactUsRow_3";
+import ContactUsRow_1 from "../organisms/contactUs/ContactUsRow_1";
 
 const GetAnEstimate = () => {
   return (
     <div>
-      <div className='getAndEstimate'>
-        <div className='getAndEstimate-content'>
+      <div className="container">
+        <div className="getAndEstimate-content">
           <ContactUsRow_1 />
           <GetAnEstimateRow_2 />
           <ContactUsRow_3 />
-
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default GetAnEstimate
+export default GetAnEstimate;
