84d37ee407ba450a00c9890da78e358a0f8603b1
RichardEcosm <richard_ecos@hotmail.com>
2023-10-27 18:16:21 -0400

    Adding loader page

--- src/components/organisms/home/HomeRow_4.jsx
+++ src/components/organisms/home/HomeRow_4.jsx
@@ -1,39 +1,27 @@
-import '../../Styles/pages/home/HomeRow_4.css';
-
-import television_academy from '../../../assets/organisms/home/homeRow_4/television-academy.png';
-import evernote from '../../../assets/organisms/home/homeRow_4/evernote.png';
-import bny_mellon from '../../../assets/organisms/home/homeRow_4/bny-mellon.png';
-import sony_pictures from '../../../assets/organisms/home/homeRow_4/sony-pictures.png';
-import history_channel from '../../../assets/organisms/home/homeRow_4/history-channel.png';
-import nissan from '../../../assets/organisms/home/homeRow_4/nissan.png';
-import vts from '../../../assets/organisms/home/homeRow_4/vts.png';
-import spredfast from '../../../assets/organisms/home/homeRow_4/spredfast.png';
-import elementum from '../../../assets/organisms/home/homeRow_4/elementum.png';
-import visa from '../../../assets/organisms/home/homeRow_4/visa.png';
-
+import "../../Styles/pages/home/HomeRow_4.css";
+import BrandsTrustUs from "../../../constants/BrandsTrustUs";
 
 const HomeRow_4 = () => {
-    return (
-        <div className='slideInUp'>
-            <div className='row-4'>
-                <div className="row-4-container">
-                    <div className="row-4-title-text"><h3 className="row-4-title">BRANDS THAT TRUST US</h3></div>
-                    <div className="row-4-images-container">
-                        <img className="row-4-img-1" src={television_academy} />
-                        <img className="row-4-img-2" src={evernote} />
-                        <img className="row-4-img-3" src={bny_mellon} />
-                        <img className="row-4-img-4" src={sony_pictures} />
-                        <img className="row-4-img-5" src={history_channel} />
-                        <img className="row-4-img-6" src={nissan} />
-                        <img className="row-4-img-7" src={vts} />
-                        <img className="row-4-img-8" src={spredfast} />
-                        <img className="row-4-img-9" src={elementum} />
-                        <img className="row-4-img-10" src={visa} />
-                    </div>
+  return (
+    <div className="slideInUp">
+      <div className="row-4">
+        <div className="row-4-container">
+          <div className="row-4-title-text">
+            <h3 className="row-4-title">BRANDS THAT TRUST US</h3>
+          </div>
+          <div className="row-4-images-container">
+            {BrandsTrustUs.map((brand) => {
+              return (
+                <div key={brand.id}>
+                  <img className="row-4-img-1" src={brand.src} />
                 </div>
-            </div>
+              );
+            })}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default HomeRow_4;
---
+++ src/constants/BrandsTrustUs.jsx
@@ -0,0 +1,55 @@
+import television_academy from "../assets/organisms/home/homeRow_4/television-academy.png";
+import evernote from "../assets/organisms/home/homeRow_4/evernote.png";
+import bny_mellon from "../assets/organisms/home/homeRow_4/bny-mellon.png";
+import sony_pictures from "../assets/organisms/home/homeRow_4/sony-pictures.png";
+import history_channel from "../assets/organisms/home/homeRow_4/history-channel.png";
+import nissan from "../assets/organisms/home/homeRow_4/nissan.png";
+import vts from "../assets/organisms/home/homeRow_4/vts.png";
+import spredfast from "../assets/organisms/home/homeRow_4/spredfast.png";
+import elementum from "../assets/organisms/home/homeRow_4/elementum.png";
+import visa from "../assets/organisms/home/homeRow_4/visa.png";
+
+const BrandsTrustUs = [
+  {
+    id: 91,
+    src: television_academy,
+  },
+  {
+    id: 92,
+    src: evernote,
+  },
+  {
+    id: 93,
+    src: bny_mellon,
+  },
+  {
+    id: 94,
+    src: sony_pictures,
+  },
+  {
+    id: 95,
+    src: history_channel,
+  },
+  {
+    id: 96,
+    src: nissan,
+  },
+  {
+    id: 97,
+    src: vts,
+  },
+  {
+    id: 98,
+    src: spredfast,
+  },
+  {
+    id: 99,
+    src: elementum,
+  },
+  {
+    id: 90,
+    src: visa,
+  },
+];
+
+export default BrandsTrustUs;
