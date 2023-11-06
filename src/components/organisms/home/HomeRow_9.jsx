22f6a8a9420cacd69792f146535049704609ac64
RichardEcosm <richard_ecos@hotmail.com>
2023-11-06 19:06:10 -0400

    Add implementation to add accordion

--- src/components/organisms/home/HomeRow_9.jsx
+++ src/components/organisms/home/HomeRow_9.jsx
@@ -1,13 +1,76 @@
+import React, { useState } from "react";
 import TechStacks from "../../../constants/TechStacks";
 import "../../Styles/pages/home/HomeRow_9.css";
 
 const HomeRow_9 = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
   return (
     <>
       <div className="row-9">
         <div className="row-9-container">
           <h3 className="row-9-title">OUR TECH STACK</h3>
           <div className="row-9-images-container">
+            <div className="mb-2">
+              <header onClick={() => setIsCollapsed(!isCollapsed)}>
+                <div className="row-9-images-container">
+                  {TechStacks.map((tech) => {
+                    if (tech.index < 10) {
+                      return (
+                        <div key={tech.id}>
+                          <img className="row-9-img-1" src={tech.src} />
+                        </div>
+                      );
+                    }
+                  })}
+                </div>
+                <div
+                  className={`collapse__content notification ${
+                    isCollapsed ? "collapsed" : "expanded"
+                  }`}
+                  aria-expanded={isCollapsed}
+                >
+                  <div className="row-9-images-container">
+                    {TechStacks.map((tech) => {
+                      if (tech.index > 9) {
+                        return (
+                          <div key={tech.id}>
+                            <img className="row-9-img-1" src={tech.src} />
+                          </div>
+                        );
+                      }
+                    })}
+                  </div>
+                </div>
+                <div
+                  style={{
+                    alignContent: "center",
+                    justifyContent: "center",
+                    marginLeft: "50%",
+                  }}
+                >
+                  {isCollapsed ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="1em"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="1em"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
+                    </svg>
+                  )}
+                </div>
+              </header>
+            </div>
+          </div>
+          {/* <div className="row-9-images-container">
             {TechStacks.map((tech) => {
               return (
                 <div key={tech.id}>
@@ -15,7 +78,7 @@ const HomeRow_9 = () => {
                 </div>
               );
             })}
-          </div>
+          </div> */}
         </div>
       </div>
     </>
--- src/components/organisms/services/ServicesRow_5.jsx
+++ src/components/organisms/services/ServicesRow_5.jsx
@@ -1,64 +1,80 @@
-import React from 'react'
+import React from "react";
 
-import react from '../../../assets/organisms//home/homeRow_9/react.png';
-import node from '../../../assets/organisms//home/homeRow_9/node.png';
-import microsoftnet from '../../../assets/organisms//home/homeRow_9/microsoftnet.png';
-import rails from '../../../assets/organisms//home/homeRow_9/rails.png';
-import java from '../../../assets/organisms//home/homeRow_9/java.png';
-import xamarin from '../../../assets/organisms//home/homeRow_9/xamarin.png';
-import microsoftazure from '../../../assets/organisms//home/homeRow_9/microsoft-azure.png';
-import reactnative from '../../../assets/organisms//home/homeRow_9/react-native.png';
-import ios_1 from '../../../assets/organisms//home/homeRow_9/ios-1.png';
-import se_1 from '../../../assets/organisms//home/homeRow_9/se-1.png';
-import ionic from '../../../assets/organisms//home/homeRow_9/ionic.png';
-import google_cloud from '../../../assets/organisms//home/homeRow_9/google-cloud.png';
-import go_1 from '../../../assets/organisms//home/homeRow_9/go-1.png';
-import backbone from '../../../assets/organisms//home/homeRow_9/backbone.png';
-import appium_2 from '../../../assets/organisms//home/homeRow_9/appium-2.png';
-import angularjs from '../../../assets/organisms//home/homeRow_9/angularjs.png';
-import android from '../../../assets/organisms//home/homeRow_9/android.png';
-import amazon from '../../../assets/organisms//home/homeRow_9/amazon.png';
-import python from '../../../assets/organisms//home/homeRow_9/python.png';
-import php from '../../../assets/organisms//home/homeRow_9/php.png';
+import react from "../../../assets/organisms//home/homeRow_9/react.png";
+import node from "../../../assets/organisms//home/homeRow_9/node.png";
+import microsoftnet from "../../../assets/organisms//home/homeRow_9/microsoftnet.png";
+import rails from "../../../assets/organisms//home/homeRow_9/rails.png";
+import java from "../../../assets/organisms//home/homeRow_9/java.png";
+import xamarin from "../../../assets/organisms//home/homeRow_9/xamarin.png";
+import microsoftazure from "../../../assets/organisms//home/homeRow_9/microsoft-azure.png";
+import reactnative from "../../../assets/organisms//home/homeRow_9/react-native.png";
+import ios_1 from "../../../assets/organisms//home/homeRow_9/ios-1.png";
+import se_1 from "../../../assets/organisms//home/homeRow_9/se-1.png";
+import ionic from "../../../assets/organisms//home/homeRow_9/ionic.png";
+import google_cloud from "../../../assets/organisms//home/homeRow_9/google-cloud.png";
+import go_1 from "../../../assets/organisms//home/homeRow_9/go-1.png";
+import backbone from "../../../assets/organisms//home/homeRow_9/backbone.png";
+import appium_2 from "../../../assets/organisms//home/homeRow_9/appium-2.png";
+import angularjs from "../../../assets/organisms//home/homeRow_9/angularjs.png";
+import android from "../../../assets/organisms//home/homeRow_9/android.png";
+import amazon from "../../../assets/organisms//home/homeRow_9/amazon.png";
+import python from "../../../assets/organisms//home/homeRow_9/python.png";
+import php from "../../../assets/organisms//home/homeRow_9/php.png";
 
-import '../../Styles/pages/services/ServicesRow_5.css'
-
+import "../../Styles/pages/services/ServicesRow_5.css";
 
 const ServicesRow_5 = () => {
-    return (
-        <div>
-            <div className="s-row-5">
-                <div className="s-row-5-container">
-                    <div className="s-row-5-title">
-                        <h3>OUR TECH STACK</h3>
-                        <div className='s-row-2-line'></div>
-                    </div>
-                    <div className="s-row-5-images-container">
-                        <img src={react} className="s-row-5-img-1" />
-                        <img src={node} className="s-row-5-img-2" />
-                        <img src={microsoftnet} className="s-row-5-img-3" />
-                        <img src={rails} className="s-row-5-img-4" />
-                        <img src={java} className="s-row-5-img-5" />
-                        <img src={xamarin} className="s-row-5-img-6" />
-                        <img src={microsoftazure} className="s-row-5-img-7" />
-                        <img src={reactnative} className="s-row-5-img-8" />
-                        <img src={ios_1} className="s-row-5-img-9" />
-                        <img src={se_1} className="s-row-5-img-10" />
-                        <img src={ionic} className="s-row-5-img-11" />
-                        <img src={google_cloud} className="s-row-5-img-12" />
-                        <img src={go_1} className="s-row-5-img-13" />
-                        <img src={backbone} className="s-row-5-img-14" />
-                        <img src={appium_2} className="s-row-5-img-15" />
-                        <img src={angularjs} className="s-row-5-img-16" />
-                        <img src={android} className="s-row-5-img-17" />
-                        <img src={amazon} className="s-row-5-img-18" />
-                        <img src={python} className="s-row-5-img-19" />
-                        <img src={php} className="s-row-5-img-20" />
-                    </div>
-                </div>
+  const [isCollapsed, setIsCollapsed] = useState(collapsed);
+  return (
+    <div>
+      <div className="s-row-5">
+        <div className="s-row-5-container">
+          <div className="s-row-5-title">
+            <h3>OUR sss TECH STACK</h3>
+            <div className="s-row-2-line"></div>
+          </div>
+          <div className="s-row-5-images-container">
+            <div className="mb-2">
+              <header onClick={() => setIsCollapsed(!isCollapsed)}>
+                <h3>2313123</h3>
+                {isCollapsed ? "+" : "-"}
+              </header>
+              <div
+                className={`collapse__content notification ${
+                  isCollapsed ? "collapsed" : "expanded"
+                }`}
+                aria-expanded={isCollapsed}
+              >
+                <p>testing</p>
+              </div>
             </div>
+          </div>
+          <div className="s-row-5-images-container">
+            <img src={react} className="s-row-5-img-1" />
+            <img src={node} className="s-row-5-img-2" />
+            <img src={microsoftnet} className="s-row-5-img-3" />
+            <img src={rails} className="s-row-5-img-4" />
+            <img src={java} className="s-row-5-img-5" />
+            <img src={xamarin} className="s-row-5-img-6" />
+            <img src={microsoftazure} className="s-row-5-img-7" />
+            <img src={reactnative} className="s-row-5-img-8" />
+            <img src={ios_1} className="s-row-5-img-9" />
+            <img src={se_1} className="s-row-5-img-10" />
+            <img src={ionic} className="s-row-5-img-11" />
+            <img src={google_cloud} className="s-row-5-img-12" />
+            <img src={go_1} className="s-row-5-img-13" />
+            <img src={backbone} className="s-row-5-img-14" />
+            <img src={appium_2} className="s-row-5-img-15" />
+            <img src={angularjs} className="s-row-5-img-16" />
+            <img src={android} className="s-row-5-img-17" />
+            <img src={amazon} className="s-row-5-img-18" />
+            <img src={python} className="s-row-5-img-19" />
+            <img src={php} className="s-row-5-img-20" />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ServicesRow_5
+export default ServicesRow_5;
