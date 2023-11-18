f501e0d78f1188ea2ffb424cfd68584dfc80cfc7
RichardEcosm <richard_ecos@hotmail.com>
2023-11-18 00:08:50 -0400

    Updating code to send email

--- src/components/molecules/contactUS/Formulary.jsx
+++ src/components/molecules/contactUS/Formulary.jsx
@@ -1,49 +1,49 @@
+import React from "react";
+import emailjs from "emailjs-com";
 
-import '../../Styles/molecules/contactUs/Formulary.css'
+import "../../Styles/molecules/contactUs/Formulary.css";
 
 const Formulary = () => {
+  function sendEmail(e) {
+    e.preventDefault(); //This is important, i'm not sure why, but the email won't send without it
 
+    emailjs
+      .sendForm(
+        "service_4ia0mi6",
+        "template_5ijot8o",
+        e.target,
+        "TrjOVdR2VR81NJsUb"
+      )
+      .then(
+        (result) => {
+          window.location.reload(); //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 
-    return (
-        <div className="formulary">
-            <h1>Send us a brief message and we will get in touch with you for scheduling a short conversation with our experts. </h1>
-            <form action="https://formsubmit.co/captiva.software@gmail.com" method="POST" >
-                <label className="formulary-content">First name:
-                    <input name="first-name" type="text" inputMode="text"
-                        autoComplete="give-name" required/>
+  return (
+    <div className="formulary">
+      <h1>
+        Send us a brief message and we will get in touch with you for scheduling
+        a short conversation with our experts.{" "}
+      </h1>
+      <form onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number" />
+        <label>Name</label>
+        <input type="text" name="from_name" />
+        <label>Email</label>
+        <input type="email" name="from_email" />
+        <label>Subject</label>
+        <input type="text" name="subject" />
+        <label>Message</label>
+        <textarea name="html_message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
 
-                </label>
-                <label className="formulary-content">Last name:
-                    <input name="last-name" type="text" inputMode="text"
-                        autoComplete="give-name" required />
-                </label>
-                <label className="formulary-content">Work Email:
-                    <input name="work-email" type="text" inputMode="text"
-                         autoComplete="give-name" required/>
-                </label>
-                <label className="formulary-content">Phone number
-                    <input name="phone-number" type="text" inputMode="text"
-                        autoComplete="give-name" required/>
-                </label>
-                <label className="formulary-content">Role
-                    <input name="role" type="text" inputMode="text"
-                         autoComplete="give-name" required/>
-                 </label>
-                <label className="formulary-content">Tell us about your project
-                    <input name="Tell-us" placeholder type="text" inputMode="text"
-                        autoComplete="give-name" required/>
-                </label>
-                
-                <button type="submit" value="Enviar">Enviar</button>
-
-
-                <input type="hidden" name="_next" value= "http://localhost:5173"/>
-                <input type="hidden" name="_captcha" value="false" />
-            </form>
-        </div>
-
-
-    )
-}
-
-export default Formulary
+export default Formulary;
