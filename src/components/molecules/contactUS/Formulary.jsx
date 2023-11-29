3aaf0f36887e9482d845643d45ad9fbb947de61d
RichardEcosm <richard_ecos@hotmail.com>
2023-11-28 20:16:31 -0400

    Adding validation for the Contact form

--- src/components/molecules/contactUS/Formulary.jsx
+++ src/components/molecules/contactUS/Formulary.jsx
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
 
 import "../../Styles/molecules/contactUs/Formulary.css";
 
 const Formulary = () => {
-  function sendEmail(e) {
-    e.preventDefault(); //This is important, i'm not sure why, but the email won't send without it
+  const initialValues = {
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    subject: "",
+    message: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [error, setError] = useState({});
 
-    emailjs
-      .sendForm(
-        "service_4ia0mi6",
-        "template_5ijot8o",
-        e.target,
-        "TrjOVdR2VR81NJsUb"
-      )
-      .then(
-        (result) => {
-          window.location.reload(); //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.fullName) {
+      errors.fullName = "FULL NAME is required";
+    }
+    if (!values.email) {
+      errors.email = "EMAIL is required";
+    }
+    if (!values.phoneNumber) {
+      errors.phoneNumber = "PHONE NUMBER is required";
+    }
+    if (!values.subject) {
+      errors.subject = "SUBJECT is required";
+    }
+    if (!values.message) {
+      errors.message = "MESSAGE is required";
+    }
+
+    return errors;
+  };
+
+  const isInvalidForm = () => {
+    return Object.values(formValues).some((x) => x === null || x === "");
+  };
+
+  function sendEmail(e) {
+    e.preventDefault();
+    setError(validate(formValues));
+    if (isInvalidForm() == false) {
+      emailjs
+        .sendForm(
+          "service_4ia0mi6",
+          "template_5ijot8o",
+          e.target,
+          "TrjOVdR2VR81NJsUb"
+        )
+        .then(
+          (result) => {
+            window.location.reload();
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
   }
 
   return (
@@ -38,49 +80,63 @@ const Formulary = () => {
               <label className="app-form-label">FULL NAME</label>
               <input
                 className="app-form-control"
-                name="form_name"
+                name="fullName"
                 placeholder="FULL NAME"
-                required
+                value={formValues.fullName}
+                onChange={handleChange}
               />
+              <span className="app-form-label error-span">
+                {error.fullName}
+              </span>
             </div>
             <div className="app-form-group">
               <label className="app-form-label">EMAIL</label>
               <input
                 className="app-form-control"
-                name="form_email"
+                name="email"
                 placeholder="EMAIL"
                 type="email"
-                required
+                value={formValues.email}
+                onChange={handleChange}
               />
+              <span className="app-form-label error-span">{error.email}</span>
             </div>
             <div className="app-form-group">
               <label className="app-form-label">PHONE NUMBER</label>
               <input
                 className="app-form-control"
-                name="form_phone_number"
+                name="phoneNumber"
                 placeholder="59179779797"
                 type="tel"
                 pattern="[0-9]{11,19}"
-                required
+                value={formValues.phoneNumber}
+                onChange={handleChange}
               />
+              <span className="app-form-label  error-span">
+                {error.phoneNumber}
+              </span>
             </div>
             <div className="app-form-group">
               <label className="app-form-label">SUBJECT</label>
               <input
                 className="app-form-control"
-                name="form_subject"
+                name="subject"
                 placeholder="SUBJECT"
-                required
+                value={formValues.subject}
+                onChange={handleChange}
               />
+              <span className="app-form-labe error-span">{error.subject}</span>
             </div>
             <div className="app-form-group message">
               <label className="app-form-label">MESSAGE</label>
               <textarea
                 className="app-form-control message"
-                name="form_message"
+                name="message"
                 placeholder="TELL US ABOUT YOURSELF"
-                required
+                value={formValues.message}
+                onChange={handleChange}
               />
+              <span className="app-form-label error-span">{error.message}</span>
             </div>
           </div>
           <input className="app-form-input" type="submit" value="Send" />
