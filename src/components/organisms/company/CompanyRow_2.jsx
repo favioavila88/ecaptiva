ff323569f161a4d86f208ed8e2c2f34352fe9249
RichardEcosm <richard_ecos@hotmail.com>
2023-11-28 22:25:18 -0400

    Updating code to align body for COMPANY

--- src/components/organisms/company/CompanyRow_2.jsx
+++ src/components/organisms/company/CompanyRow_2.jsx
@@ -1,26 +1,65 @@
-import React from 'react'
+import React from "react";
 
-import '../../Styles/pages/company/CompanyRow_2.css'
+import "../../Styles/pages/company/CompanyRow_2.css";
 
-import qaLeft from '../../../assets/organisms//home/homeRow_5/quotation-left-1.png'
-import qaRight from '../../../assets/organisms//home/homeRow_5/quotation-right-1.png'
-
+import qaLeft from "../../../assets/organisms//home/homeRow_5/quotation-left-1.png";
+import qaRight from "../../../assets/organisms//home/homeRow_5/quotation-right-1.png";
 
 const CompanyRow_2 = () => {
   return (
-    <div className='cp-row-2'>
-      <div className='cp-row-2-container'>
-        <div className='cp-row-2-content-left'>
-          <img src={qaLeft} alt="qaLeft" className='qaLeft' />
-          <h3>Through high-end processes and engineers, we provide the service and
-            transparency our clients need.</h3>
-          <h4 className='name-ceo'>Christian Alvaro Avila Perez –</h4>
-          <h4 className='post'> CEO</h4>
-          <img src={qaRight} alt="qaRight" className='qaRight' />
-        </div>
-      </div>
-    </div>
-  )
-}
+    <div
+      className="cp-row-2"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "15% 70% 15%",
+        gridTemplateRows: "100%",
+        gridGap: "10px",
+      }}
+    >
+      <>
+        <div></div>
 
-export default CompanyRow_2
+        <div
+          className="cp-row-2-container"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "10% 80% 10%",
+            gridTemplateRows: "100%",
+            gridGap: "10px",
+          }}
+        >
+          <>
+            <div>
+              <img src={qaLeft} alt="qaLeft" className="qaLeft" />
+            </div>
+            <div>
+              <div className="cp-row-2-content-left">
+                <h3>
+                  Through high-end processes and engineers, we provide the
+                  service and transparency our clients need.
+                </h3>
+              </div>
+              <h4 className="name-ceo">Christian Alvaro Avila Perez –</h4>
+              <h4 className="post"> CEO</h4>
+            </div>
+            <div>
+              <img src={qaRight} alt="qaRight" className="qaRight" />
+            </div>
+          </>
+          {/* <div className="cp-row-2-content-left">
+              <img src={qaLeft} alt="qaLeft" className="qaLeft" />
+              <h3>
+                Through high-end processes and engineers, we provide the service
+                and transparency our clients need.
+              </h3>
+              <h4 className="name-ceo">Christian Alvaro Avila Perez –</h4>
+              <h4 className="post"> CEO</h4>
+              <img src={qaRight} alt="qaRight" className="qaRight" />
+            </div> */}
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default CompanyRow_2;
--- src/components/organisms/home/HomeRow_4.jsx
+++ src/components/organisms/home/HomeRow_4.jsx
@@ -5,19 +5,32 @@ const HomeRow_4 = () => {
   return (
     <div className="slideInUp">
       <div className="row-4">
-        <div className="row-4-container">
-          <div className="row-4-title-text">
-            <h3 className="row-4-title">BRANDS THAT TRUST US</h3>
-          </div>
-          <div className="row-4-images-container">
-            {BrandsTrustUs.map((brand) => {
-              return (
-                <div key={brand.id}>
-                  <img className="row-4-img-1" src={brand.src} />
-                </div>
-              );
-            })}
-          </div>
+        <div
+          className="row-4-container"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "15% 70% 15%",
+            gridTemplateRows: "100%",
+            gridGap: "10px",
+          }}
+        >
+          <>
+            <div></div>
+            <div>
+              <div className="row-4-title-text">
+                <h3 className="row-4-title">BRANDS THAT TRUST US</h3>
+              </div>
+              <div className="row-4-images-container">
+                {BrandsTrustUs.map((brand) => {
+                  return (
+                    <div key={brand.id}>
+                      <img className="row-4-img-1" src={brand.src} />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </>
         </div>
       </div>
     </div>
